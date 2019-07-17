3d42bbbd74c55a73b5b568c04b443f6b1372422f
SaishNarvekar <35778345+SaishNarvekar@users.noreply.github.com>
2019-07-17 11:06:10 +0530

    Initial Commit

---
+++ cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+
+function dirTree(filename) {
+    var stats = fs.lstatSync(filename);
+    var info = {
+        path: filename,
+        name: path.basename(filename)
+    };
+    if (stats.isDirectory()) {
+        info.type = "folder";
+        info.children = fs
+            .readdirSync(filename)
+            .map(child => dirTree(path.join(filename, child)));
+    } else {
+        info.type = "file";
+    }
+    return info;
+}
+
+console.log(JSON.stringify(dirTree("."  ),null,2));
